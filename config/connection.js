e8367b1aaee3d072da6a2f7f187b78a8c12bbde1
won518 <sewonpark518@gmail.com>
2019-06-03 22:43:21 -0700

    fixed error in connection.js

--- config/connection.js
+++ config/connection.js
@@ -16,13 +16,7 @@ if(process.env.JAWSDB_URL){
   
 }
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1511",
-  database: "burgers_db"
-});
+
 
 // Make connection.
 connection.connect(function(err) {
